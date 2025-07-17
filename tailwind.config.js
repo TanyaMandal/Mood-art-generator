342db565551fb214360a1e27c364ab5f60389d00
vanshika garg <156303524+0vanshika@users.noreply.github.com>
2025-07-17 23:08:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan all JS/JSX/TS/TSX files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
